3c1b09cd8353eecba4f5b9910efc789126939290
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-05-02 01:02:47 +0900

    ログインテーブルにcompany_idを追加

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -93,16 +93,18 @@ DROP TABLE IF EXISTS admin;
 CREATE TABLE admin
 (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  company_id INT NOT NULL,
   password VARCHAR(255) NOT NULL,
-  mail_admin VARCHAR(255) NOT NULL
+  mail_admin VARCHAR(255) NOT NULL,
+  delete_flg INT NOT NULL
 );
 
-INSERT INTO admin (password, mail_admin) VALUES 
-('aaaaaa', 'ssssmmmmllll@gmail.com'),
-('bbbbbb', 'mmmmssssllll@gmail.com'),
-('cccccc', 'ssssllllmmmm@gmail.com'),
-('dddddd', 'mmssssmmllll@gmail.com'),
-('eeeeee', 'llllssssmmmm@gmail.com');
+INSERT INTO admin (password, company_id, mail_admin, delete_flg) VALUES 
+('aaaaaa', 1, 'ssssmmmmllll@gmail.com', 0),
+('bbbbbb', 2, 'mmmmssssllll@gmail.com', 0),
+('cccccc', 3, 'ssssllllmmmm@gmail.com', 0),
+('dddddd', 4, 'mmssssmmllll@gmail.com', 0),
+('eeeeee', 5, 'llllssssmmmm@gmail.com', 0);
 
 
 -- 学生・エージェント中間テーブル
