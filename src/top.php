8f30ce832aebb1bab696e5904ac2728d820016d7
sanukiudon27 <uudodo0207@icloud.com>
2022-05-06 13:51:01 +0900

    文字化けに日時未実装だが最低限

---
+++ src/top.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+  <main>
+    <section id="company">
+      <div class="company_wrapper">
+        <h2>企業一覧</h2>
+        <div class="company_list enclosing_line">
+          <!-- 一つの会社ボックス -->
+          <div class="company_box">
+            <div class="company_box_logo">
+              <img src="" alt="">
+            </div>
+            <div class="company_box_img">
+              <img src="" alt="">
+            </div>
+            <div class="company_box_info">
+              <div class="company_info_first">
+                <img src="" alt="">
+                <p>IT</p>
+                <p>商社</p>
+              </div>
+              <div class="company_info_second">
+                <img src="" alt="">
+                <p>満足度97%</p>
+              </div>
+              <div class="company_info_third">
+                <img src="" alt="">
+                <!-- ここら辺もしphpなら二つのp要素くっつけてもいいかもです -->
+                <p>文系</p>
+                <p>体育会系</p>
+                <!-- ここまで -->
+              </div>
+            </div>
+            <div class="company_box_exp">
+              <p>マイナビ新卒紹介は…</p>
+            </div>
+            <div class="company_box_button">
+              <a href="" class="inquiry">お問い合わせはこちら</a>
+              <a href="" class="comparison">複数の会社を比較する</a>
+            </div>
+            <div class="company_box_check">
+              <label for="check"><input type="checkbox">選択する</label>
+            </div>
+          </div>
+          <!-- ここまで -->
+        </div>
+      </div>
+    </section>
+  </main>
+</body>
+
+</html>
