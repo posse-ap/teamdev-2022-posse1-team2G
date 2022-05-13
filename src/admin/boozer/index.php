4ffdfceb8437bd37d05a6e4462289d25279fae03
sanukiudon27 <uudodo0207@icloud.com>
2022-05-13 11:52:20 +0900

    fetch.phpからDBの値を反映に成功

--- src/admin/boozer/index.php
+++ src/admin/boozer/index.php
@@ -30,6 +30,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
 <!-- </body> -->
+
 <body>
 
   <div class="container mt-5">
@@ -60,7 +61,18 @@
                 </tr>
               </thead>
               <tbody class="studentdata">
-
+                <!-- <tr>
+                  <td>1</td>
+                  <td>Ved</td>
+                  <td>Ve</td>
+                  <td>Vaaa</td>
+                  <td>C</td>
+                  <td>
+                    <a href="" class='badge btn-info'>VIEW</a>
+                    <a href="" class='badge btn-primary'>EDIT</a>
+                    <a href="" class='badge btn-danger'>DELETE</a>
+                  </td>
+                </tr> -->
               </tbody>
             </table>
           </div>
@@ -71,7 +83,9 @@
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  
   <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
@@ -86,15 +100,18 @@
       getdata();
     });
 
+    // fetch.phpと連携してデータを持ってくる
     function getdata() {
       $.ajax({
         type: "GET",
-        url: "ajax-crud/fetch.php",
+// fetch.phpからデータを持ってくる
+        url: "./fetch.php",
         success: function(response) {
           // console.log(response);
           $.each(response, function(key, value) {
             // console.log(value['fname']);
             $('.studentdata').append('<tr>' +
+            // valueの値はキー
               '<td class="stud_id">' + value['id'] + '</td>\
                                 <td>' + value['fname'] + '</td>\
                                 <td>' + value['lname'] + '</td>\
