8697b5fca5a1b7bf6dca222354346ff845d57919
sanukiudon27 <uudodo0207@icloud.com>
2022-05-20 14:49:14 +0900

    階層関係で反映されないから一回落とす

--- src/admin/agent/information_posting.php
+++ src/admin/agent/information_posting.php
@@ -9,8 +9,11 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.css" />
-  <link rel="stylesheet" href="../admin.css">
+
+
+
   <!-- <link rel="stylesheet" href="../../style.css"> -->
+  <link rel="stylesheet" href="../../admin/admin.css">
 
 
   <title>各企業詳細掲載ページ</title>
