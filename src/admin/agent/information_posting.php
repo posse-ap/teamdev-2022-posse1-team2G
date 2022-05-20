e6e6d5f550db4f415fcce6142054c45d1e68212d
sanukiudon27 <uudodo0207@icloud.com>
2022-05-20 14:27:32 +0900

    階層関係で反映されない

---
+++ src/admin/agent/information_posting.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.css" />
+  <link rel="stylesheet" href="../admin.css">
+  <!-- <link rel="stylesheet" href="../../style.css"> -->
+
+
+  <title>各企業詳細掲載ページ</title>
+</head>
+
+<body>
+  <div>
+    <div class='outline'>
+      <h1 class='aaa'>企業掲載情報</h1>
+    </div>
+
+    <div>
+      <h2>会社情報</h2>
+      <dl class="row">
+        <dt class="col-sm-3">会社名</dt>
+        <dd class="col-sm-9">○○会社</dd>
+        <dt class="col-sm-3">メールアドレス</dt>
+        <dd class="col-sm-9">
+          <p>a@cavav.com</p>
+          <p>a@dva.com</p>
+        </dd>
+        <dt class="col-sm-3">別の用語</dt>
+        <dd class="col-sm-9">この別の用語の定義。</dd>
+        </dd>
+      </dl>
+    </div>
+
+    <dl class="row">
+      <dt class="col-sm-3">説明リスト</dt>
+      <dd class="col-sm-9">説明リストは、用語を定義するのに最適。</dd>
+      <dt class="col-sm-3">用語</dt>
+      <dd class="col-sm-9">
+        <p>用語の定義。</p>
+        <p>同じ用語の2番目の定義。</p>
+      </dd>
+      <dt class="col-sm-3">別の用語</dt>
+      <dd class="col-sm-9">この別の用語の定義。</dd>
+      </dd>
+    </dl>
+
+
+  </div>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+  <script src="./user_list.js"></script>
+
+
+</body>
+
+</html>
