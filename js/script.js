f0fbb84cf24192057e179fd202f8a94b09cd47a9
mei <meilate0211@gmail.com>
2022-05-22 15:21:45 +0900

    企業一覧編集

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(function () {
   /*タイトルをクリックすると*/
   $(".question_box").on("click", function() {
-　　 /*クリックした隣の要素を開閉する*/
+    /*クリックした隣の要素を開閉する*/
     $(this).next().slideToggle(300);
-　　/*タイトルにopenクラスの追加、削除を行って矢印の向きを変える*/
+    /*タイトルにopenクラスの追加、削除を行って矢印の向きを変える*/
     $(this).toggleClass("open",300);
   });
 });
