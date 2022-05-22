9f28bb5ed3fac3d5b606f9bc96e6569a8b59e41e
sanukiudon27 <uudodo0207@icloud.com>
2022-05-22 23:56:38 +0900

    loginのフロント

--- src/admin/login.php
+++ src/admin/login.php
@@ -65,41 +65,68 @@ if (!empty($_POST)) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./normalize.css">
+  <link rel="stylesheet" href="../admin/admin_style.css">
   <title>管理者ログイン</title>
 </head>
 
-<body>
+<body class='login_body'>
   <div>
-    <h1>管理者ログイン</h1>
+    <div class='login_logo'>
+      <img src="../../img/boozer_logo.png" alt="logo">
+      <img src="../../img/boozer_logo (1).png" alt="期待">
+      <!-- <img src="../boozer_logo.png" alt="logo"> -->
+      <!-- <img src="..\..\img\boozer_logo.png" alt="aa"> -->
+    </div>
+    <div class='login_container'>
+      <div class='login_title'>
+        <h1>Login</h1>
+      </div>
+      <?php if ($errormessage) { ?>
+        <ul class='login_error'>
+          <!-- $errorは連想配列なのでforeachで分解していく -->
+          <?php foreach ($errormessage as $value) { ?>
+            <li><?php echo $value; ?></li>
+          <?php } ?>
+          <!-- 分解したエラー文をlistの中に表示していく -->
+        </ul>
+      <?php } ?>
 
-    <?php if ($errormessage) { ?>
-      <ul>
-        <!-- $errorは連想配列なのでforeachで分解していく -->
-        <?php foreach ($errormessage as $value) { ?>
-          <li><?php echo $value; ?></li>
-        <?php } ?>
-        <!-- 分解したエラー文をlistの中に表示していく -->
-      </ul>
-    <?php } ?>
-
-    <form action="/admin/login.php" method="POST" class='box'>
-      <h1>Login</h1>
-      <input type="email" name="email" placeholder="Username">
-      パスワード<input type="password" name="password" placeholder="password">
-      <input type="submit" value="ログイン">
-    </form>
-    <!-- <a href="/index.php">イベント一覧</a> -->
-    <a href="./forgetPass.php">パスワードをお忘れの方</a>
+      <form action="/admin/login.php" method="POST" class='login_form'>
+        <div class='login_mail'>
+          <div>
+            <p>メールアドレス</p>
+          </div>
+          <div class='mail_container'>
+            <input type="email" name="email">
+          </div>
+        </div>
+        <div class='login_pass'>
+          <div>
+            <p>パスワード</p>
+          </div>
+          <div>
+            <input type="password" name="password">
+          </div>
+        </div>
+        <div class='login_submit'>
+          <input type="submit" value="ログイン">
+        </div>
+        <div class='login_pass_forget'>
+          <a href="./forgetPass.php">パスワードを忘れた場合</a>
+        </div>
+      </form>
+      <!-- <a href="/index.php">イベント一覧</a> -->
+    </div>
   </div>
 </body>
 
 
-<form class="box" action="index.htyml" method="post">
+<!-- <form class="box" action="index.htyml" method="post">
   <h1>Login</h1>
   <input type="text" name="" placeholder="Username">
   <input type="password" name="" placeholder="Password">
   <input type="submit" name="" value="Login">
-</form>
+</form> -->
 
 
 </html>
